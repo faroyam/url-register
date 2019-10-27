5f2d0d258474e3edd8518d70d8abf3b51a495d24
faroyam <faroyam1@yamex.ru>
2019-10-27 11:52:53 +0700

    update mix.exs; mix.lock

--- mix.exs
+++ mix.exs
@@ -7,7 +7,12 @@ defmodule UrlRegister.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "UrlRegister",
+      source_url: "https://github.com/faroyam/url-register"
     ]
   end
 
@@ -26,7 +31,19 @@ defmodule UrlRegister.MixProject do
       {:redix, "~> 0.10.2"},
       {:castore, "~> 0.1.4"},
       {:poison, "~> 4.0.1"},
-      {:plug_logger_json, "~> 0.7.0"}
+      {:plug_logger_json, "~> 0.7.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "This application tracks URL visits and gathers the data to Redis."
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/faroyam/url-register"}
     ]
   end
 end
